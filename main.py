2ae28f769c4e0d489d95773a4b78a30f8776071d
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-01-12 23:58:31 +0200

    updated

--- main.py
+++ main.py
@@ -31,15 +31,32 @@ states_with_pos = {}
 for index, key  in enumerate(states):
     states_with_pos[key] = combine_x_y[index]
 
+correct_states = []
+
 correct = 0
 while correct != len(states_with_pos):
     
     # Bring the pop for the user to answer
-    user_choice = screen.textinput(title=f"{correct}/{len(states_with_pos)} States/correct", prompt="Enter state:").strip().rstrip().capitalize()
+    user_choice = screen.textinput(title=f"{correct}/{len(states_with_pos)} States/correct", prompt="Enter state:").strip().rstrip().title()
 
+    # check if user already given that so that I don't increment
     # Check if the user choice == something in the dict:
     if user_choice in states_with_pos.keys():
         placing_states.write_state(name_of_state=user_choice, x=states_with_pos[user_choice][0], y=states_with_pos[user_choice][1])
+        correct_states.append(user_choice)
         correct += 1
 
+    # Check for exit 
+    if user_choice == "Exit":
+        missing_states = [state for state in states_with_pos.keys() if state not in correct_states]
+
+        newdata = pd.DataFrame(missing_states)
+        newdata.to_csv("states_to_learn.csv")
+        break
+
+
+
+
+# Exit code
+# Then tell user which states they missed
 screen.exitonclick()
